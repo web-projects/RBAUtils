ef6c5953cbd7a45cc4a0b7d442c7dddd2a6248fd
Jon Bianco <web.projects@yahoo.com>
2019-04-25 15:19:40 -0700

    Add files via upload

---
+++ RBAUtils/Form1.Designer.cs
@@ -0,0 +1,145 @@
+namespace RBAUtils
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRebootTime = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(54, 47);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(662, 350);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.txtRebootTime);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(654, 324);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "24REBOOT";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(654, 324);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REBOOT TIME:";
+            // 
+            // txtRebootTime
+            // 
+            this.txtRebootTime.Location = new System.Drawing.Point(141, 34);
+            this.txtRebootTime.MaxLength = 4;
+            this.txtRebootTime.Name = "txtRebootTime";
+            this.txtRebootTime.Size = new System.Drawing.Size(107, 20);
+            this.txtRebootTime.TabIndex = 1;
+            this.txtRebootTime.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(140, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(651, 320);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtRebootTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RBAUtils/Form1.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBAUtils
+{
+    public partial class Form1 : Form
+    {
+        string rebootTime;
+        RBAUtils rbautils = new RBAUtils();
+
+        public Form1()
+        {
+            InitializeComponent();
+            rebootTime = rbautils.Get24RebootTime();
+            Debug.WriteLine("INITIAL REBOOT TIME VALUE={0}", (object)rebootTime);
+            this.txtRebootTime.Text = rebootTime;
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+            if(rebootTime.Equals(this.txtRebootTime.Text))
+            {
+                this.button1.Enabled = false;
+            }
+            else
+            {
+                this.button1.Enabled = true;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rbautils.Set24RebootTime(this.txtRebootTime.Text);
+            this.button1.Enabled = false;
+            this.pictureBox1.Visible = true;
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                Debug.WriteLine("--------------------------------------------------------------------------------------------------------------------------------");
+                Debug.Write("WAITING FOR DEVICE TO COMPLETE REBOOT[ ");
+                for(int index = 0; index < 35; index ++)
+                { 
+                    Debug.Write(".");
+                    Thread.Sleep(1000);
+                }
+                Debug.WriteLine(" ] - ONLINE");
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    rbautils = new RBAUtils();
+                    rebootTime = rbautils.Get24RebootTime();
+                    Debug.WriteLine("UPDATED REBOOT TIME VALUE={0}", (object)rebootTime);
+                    this.txtRebootTime.Text = rebootTime;
+                    this.pictureBox1.Visible = false;
+                }));
+            }).Start();;
+        }
+
+    }
+}
